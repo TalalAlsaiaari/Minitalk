2a258ef0b8ee5325f2b09e63d249cb4df55adc6f
Talal Alsaiaari <talsaiaa@lab1r1s5.42abudhabi.ae>
2022-08-18 16:06:56 +0400

    Changed usleep to 80 just to be safe

--- ft_client/ft_client.c
+++ ft_client/ft_client.c
@@ -6,7 +6,7 @@
 /*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 00:15:20 by talsaiaa          #+#    #+#             */
-/*   Updated: 2022/08/18 01:10:09 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2022/08/18 14:58:37 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	converter(char **av, int x)
 			y = y / 2;
 		}
 		counter++;
-		usleep(30);
+		usleep(80);
 	}
 }
 
